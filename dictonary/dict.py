799fc9b52e8520242b4c6a96bf01e466fc3f2039
samirvithlani <samir.vithlani83955@gmail.com>
2022-09-24 13:57:50 +0530

    dictonary

---
+++ dictonary/dict.py
@@ -0,0 +1,18 @@
+students = {2:"tom",505:"abc",3:"jerry",4:"lucy",5:"lily",1:"amit","s":"samsung",2:"tomy"}
+print(students)
+
+print(students.get('s'))
+print(students.values())
+print(students.keys())
+
+print("item",students.items())
+print(students.pop(2))
+
+print("after pop",students)
+#print key value pair data
+#x = students.values()
+#print(x)
+
+users = {1:{1:"tom",2:"jerry"}}
+print(users)
+
---
+++ tuple/tupledemo.py
@@ -0,0 +1,10 @@
+products = ("apple","apple","samsung","xiaomi","huawei","oppo","vivo","meizu",("iphone","ipad","macbook"))
+
+cnt = products.count("apple")
+print(cnt)
+len = len(products[2])
+print(len)
+
+cat = ("eletronics","fashion")
+product2 = products + cat
+print(product2)
