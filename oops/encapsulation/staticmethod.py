43fe27f1e25f0dcc792963ada2901f645a68a97a
samirvithlani <samir.vithlani83955@gmail.com>
2022-11-13 09:07:47 +0530

    static

--- oops/encapsulation/staticmethod.py
+++ oops/encapsulation/staticmethod.py
@@ -1,21 +1,34 @@
 class Employee:
+    
+    email = "samir@gmail.com"
+    
     def __init__(self,name,id):
-        self.name = name
+        self.name = name #instance variable
         self.id = id
     
     #decorator , annotation
     
+    def demo(self):
+        print(self.name)
+        print(self.id)
+    
     @classmethod
-    def printEmployeeData(self):
-        print("print data")
+    def printEmployeeData(self,name):
+        print(name)
+        print("print data",self.email)
+        #print("print data",self.name)
         
     
     #decorator 
     @staticmethod
-    def getEmployeeData():
+    def getEmployeeData(email):
+        print(email)
+        #only static variable can be used in static method
+        #print(self.name)
         print("get employee data")            
         
         
 emp = Employee("amit",101)
-emp.printEmployeeData()
-Employee.getEmployeeData()
+emp.printEmployeeData("mmmmm")
+#emp.demo()
+#Employee.getEmployeeData("samir123")
